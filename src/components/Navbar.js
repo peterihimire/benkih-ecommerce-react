6fea88647a74a15eb8bc904d7f12023436f96571
peterihimire <peterihimire@gmail.com>
2020-06-04 00:22:58 +0100

    working on the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,65 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaSearch, FaCartPlus, FaUser } from "react-icons/fa";
 
 const Navbar = props => {
   return (
-    <div>
-      <h1>This is the Navbar</h1>
-    </div>
+    <React.Fragment>
+      <nav className="navbar">
+        <div className="navbar-center">
+          <div className="navbar-header">
+            <Link className="logo">
+              <strong>
+                ben<span>kih</span>{" "}
+              </strong>
+            </Link>
+          </div>
+          <ul className="navbar-links">
+            <li className="navbar-item">
+              <Link>home</Link>
+            </li>
+            <li className="navbar-item">
+              <Link>products</Link>
+            </li>
+            <li className="navbar-item">
+              <Link>about</Link>
+            </li>
+            <li className="navbar-item">
+              <Link>contact</Link>
+            </li>
+          </ul>
+
+          <ul className="navbar-icons">
+            <li className="navbar-items items-btn">
+              <button className="navbar-btn">
+                <FaBars className="navbar-btn-icon" />
+              </button>
+            </li>
+            <li className="navbar-items">
+              <Link>
+                <span>
+                  <FaSearch />
+                </span>
+              </Link>
+            </li>
+            <li className="navbar-items">
+              <Link>
+                <span>
+                  <FaUser />
+                </span>
+              </Link>
+            </li>
+            <li className="navbar-items">
+              <Link>
+                <span>
+                  <FaCartPlus />
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </React.Fragment>
   );
 };
 
