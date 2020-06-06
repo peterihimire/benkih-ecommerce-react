ce0c5300c8787cfbb4157fd0b972db444de298a3
peterihimire <peterihimire@gmail.com>
2020-06-06 16:58:09 +0100

    The product component is almost ready both with all hover effects and transitions, add also a details pagge

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Details from "./pages/Details";
 import NotFound from "./pages/NotFound";
 import { Route, Switch } from "react-router-dom";
 
@@ -29,6 +30,7 @@ class App extends Component {
           <Route path="/products" component={Products} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
+          <Route path="/details" component={Details} />
           <Route component={NotFound} />
         </Switch>
         <Footer />
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaCartPlus } from "react-icons/fa";
 import { ProductConsumer } from "../context";
 
 function Product(props) {
-  const { id, name, price, images } = props.product;
+  const { id, name, price, images, inCart } = props.product;
   // console.log(id, name, price);
   return (
     <article className="product">
       <div
         className="img-container"
-        onClick={console.log("img-container clicked")}
+        onClick={() => console.log("img-container clicked")}
       >
         <Link to="/details">
           <img src={images[0]} alt="product" />
         </Link>
+        <button
+          className="cart-btn"
+          disabled={inCart ? true : false}
+          onClick={() => {
+            console.log("added to the cart");
+          }}
+        >
+          {inCart ? <p disabled>in cart</p> : <FaCartPlus />}
+        </button>
+      </div>
+      <div className="product-footer">
+        <p>{name}</p>
+        <h3>
+          <span>$ </span>
+          {price}
+        </h3>
       </div>
     </article>
   );
