2dde2b3564c444b1be0cb3fdb338c7be7845c14a
peterihimire <peterihimire@gmail.com>
2020-07-06 23:09:15 +0100

    Added loading component to both FeaturedProducts and NewArrival components

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -3,60 +3,64 @@ import { ProductConsumer } from "../context";
 import { Link } from "react-router-dom";
 import { FaCartPlus, FaInfo } from "react-icons/fa";
 import Title from "../components/Title";
-
+import Loading from "../components/Loading";
 const FeaturedProducts = () => {
   return (
     <ProductConsumer>
       {value => {
         console.log(value);
-        const { featured } = value;
+        const { featured, loading } = value;
         console.log(featured);
         return (
           <section className="featured">
             <div className="featured-center">
               <Title title="featured products" />
               <div className="featured-container">
-                {featured.map(featured => {
-                  return (
-                    <article className="product new-arr-product">
-                      <div
-                        className="img-container img-container-featured"
-                        onClick={() => value.detailHandler(featured.slug)}
-                      >
-                        <Link to={`/details/${featured.slug}`}>
-                          <img src={featured.image} alt="product" />
-                        </Link>
-                        <Link
-                          to={`/details/${featured.slug}`}
-                          className="cart-btn-details"
+                {loading ? (
+                  <Loading />
+                ) : (
+                  featured.map(featured => {
+                    return (
+                      <article className="product new-arr-product">
+                        <div
+                          className="img-container img-container-featured"
+                          onClick={() => value.detailHandler(featured.slug)}
                         >
-                          <FaInfo />
-                        </Link>
-                        <button
-                          className="cart-btn"
-                          disabled={featured.inCart ? true : false}
-                          onClick={() => {
-                            value.addToCart(featured.slug);
-                            value.openModalHandler(featured.slug);
-                          }}
-                        >
-                          {featured.inCart ? (
-                            <p disabled>in cart</p>
-                          ) : (
-                            <FaCartPlus />
-                          )}
-                        </button>
-                      </div>
+                          <Link to={`/details/${featured.slug}`}>
+                            <img src={featured.image} alt="product" />
+                          </Link>
+                          <Link
+                            to={`/details/${featured.slug}`}
+                            className="cart-btn-details"
+                          >
+                            <FaInfo />
+                          </Link>
+                          <button
+                            className="cart-btn"
+                            disabled={featured.inCart ? true : false}
+                            onClick={() => {
+                              value.addToCart(featured.slug);
+                              value.openModalHandler(featured.slug);
+                            }}
+                          >
+                            {featured.inCart ? (
+                              <p disabled>in cart</p>
+                            ) : (
+                              <FaCartPlus />
+                            )}
+                          </button>
+                        </div>
 
-                      <div className="new-arr-footer">
-                        {/* <p>{featured.name}</p> */}
-                        <h3>
-                          <span>$ {featured.price}</span>
-                        </h3>
-                      </div>
-                    </article>
-                  );
-                })}
+                        <div className="new-arr-footer">
+                          {/* <p>{featured.name}</p> */}
+                          <h3>
+                            <span>$ {featured.price}</span>
+                          </h3>
+                        </div>
+                      </article>
+                    );
+                  })
+                )}
               </div>
             </div>
           </section>
--- src/components/NewArrival.js
+++ src/components/NewArrival.js
@@ -3,59 +3,64 @@ import { ProductConsumer } from "../context";
 import { Link } from "react-router-dom";
 import { FaCartPlus, FaInfo } from "react-icons/fa";
 import Title from "../components/Title";
+import Loading from "../components/Loading";
 
 const NewArrival = () => {
   return (
     <ProductConsumer>
       {value => {
         console.log(value);
-        const { newArrival } = value;
+        const { newArrival, loading } = value;
         console.log(newArrival);
         return (
           <section className="featured">
             <div className="featured-center">
               <Title title="new arrivals" />
               <div className="featured-container">
-                {newArrival.map(newArr => {
-                  return (
-                    <article className="product new-arr-product">
-                      <div
-                        className="img-container img-container-newarr"
-                        onClick={() => value.detailHandler(newArr.slug)}
-                      >
-                        <img src={newArr.image} alt="product" />
-
-                        <Link
-                          to={`/details/${newArr.slug}`}
-                          className="cart-btn-details"
+                {loading ? (
+                  <Loading />
+                ) : (
+                  newArrival.map(newArr => {
+                    return (
+                      <article className="product new-arr-product">
+                        <div
+                          className="img-container img-container-newarr"
+                          onClick={() => value.detailHandler(newArr.slug)}
                         >
-                          <FaInfo />
-                        </Link>
-                        <button
-                          className="cart-btn"
-                          disabled={newArr.inCart ? true : false}
-                          onClick={() => {
-                            value.addToCart(newArr.slug);
-                            value.openModalHandler(newArr.slug);
-                          }}
-                        >
-                          {newArr.inCart ? (
-                            <p disabled>in cart</p>
-                          ) : (
-                            <FaCartPlus />
-                          )}
-                        </button>
-                      </div>
+                          <img src={newArr.image} alt="product" />
 
-                      <div className="new-arr-footer">
-                        {/* <p>{newArr.name}</p> */}
-                        <h3>
-                          <span>$ {newArr.price}</span>
-                        </h3>
-                      </div>
-                    </article>
-                  );
-                })}
+                          <Link
+                            to={`/details/${newArr.slug}`}
+                            className="cart-btn-details"
+                          >
+                            <FaInfo />
+                          </Link>
+                          <button
+                            className="cart-btn"
+                            disabled={newArr.inCart ? true : false}
+                            onClick={() => {
+                              value.addToCart(newArr.slug);
+                              value.openModalHandler(newArr.slug);
+                            }}
+                          >
+                            {newArr.inCart ? (
+                              <p disabled>in cart</p>
+                            ) : (
+                              <FaCartPlus />
+                            )}
+                          </button>
+                        </div>
+
+                        <div className="new-arr-footer">
+                          {/* <p>{newArr.name}</p> */}
+                          <h3>
+                            <span>$ {newArr.price}</span>
+                          </h3>
+                        </div>
+                      </article>
+                    );
+                  })
+                )}
               </div>
             </div>
           </section>
